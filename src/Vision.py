e0fdb8d4438dd1c4d1944cbacdb60e3ada00d54e
Nathan Harmon <nharmon@gatech.edu>
2017-04-18 20:21:24 +0100

    Particle filter adjustments

--- src/Vision.py
+++ src/Vision.py
@@ -18,7 +18,6 @@ class Tracker:
         :param target (numpy.array): template for target we're tracking
         :param img (numpy.array): first image for tracking
         :param particles (int): number of particles to use
-        
         :inst self.target (numpy.array): target template
         :inst self.img (numpy.array): current image
         :inst self.maxrw (float): current maximum raw weight
@@ -40,7 +39,6 @@ class Tracker:
         :param img1 (numpy.array): First image
         :param img2 (numpy.array): Second image
         :param sigma (float): MSE weight
-        
         :return similarity (float): <=1.0 where 1.0 means images are equal
         """
         # If different shape, throw an error
@@ -69,7 +67,6 @@ class Tracker:
         
         :param particles (numpy.array): Array of particle coordinates
         :param weights (numpy.array): Particle weights
-        
         :return (tuple): Weighted mean
         """
         u_weighted_mean = 0.
@@ -79,6 +76,29 @@ class Tracker:
             v_weighted_mean += particles[i][1] * weights[i]
 
         return (int(u_weighted_mean), int(v_weighted_mean))
+    
+    def getImageSection(self, center, shape):
+        """Return a section of the current image, of size `shape` centered at
+           `center`
+        
+        :param center (tuple): Coordinate where the subimage is centered
+        :param shape (tuple): Width and height of subimage
+        :return subimage (numpy.ndarray): Cropped image section, or False if
+                                          there is an error.
+        """
+        s_top = int(center[0] - np.floor(shape[0]/2.))
+        s_bottom = int(center[0] + np.ceil(shape[0]/2.))
+        s_left = int(center[1] - np.floor(shape[1]/2.))
+        s_right = int(center[1] + np.ceil(shape[1]/2.))
+        try:
+            subimage = self.img[s_top:s_bottom,s_left:s_right]
+        except:
+            return False
+        
+        if subimage.shape == shape:
+            return subimage
+        
+        return False
     
     def normWeights(self, weights):
         """Normalizes particle weights
@@ -97,7 +117,6 @@ class Tracker:
         
         :param particles (list): (row,col) locations of each particle
         :param weights (list): weight of each particle
-        
         :return new_particles (list): resampled particles
         """
         new_particles = []
@@ -111,6 +130,7 @@ class Tracker:
                 beta -= weights[index]
                 index = (index + 1) % N
             
+            # Adding random noise to particles
             x = particles[index][0] + random.randint(-10,10)
             y = particles[index][1] + random.randint(-10,10)
             new_particles.append([x,y])
@@ -121,7 +141,6 @@ class Tracker:
         """Guesses where our target is in the new image
         
         :param img (np.array): New image
-        
         :return center (tuple): Best guess of target's center
         """
         self.img = cv2.GaussianBlur(img,(15,15),7)
@@ -137,17 +156,14 @@ class Tracker:
         
         self.center = self.getParticleWeightedMean(self.particles, 
                                                    self.weights)
-        #TODO: Update self.target using new center. Need to test and 
-        #      validate method below. Also look into sampling resized
-        #      targets (one bigger, one smaller) to adjust for size
-        #      changes. Should we blend new image with old? How does
-        #      that degrade over time?
-        #y_top = min(0,center[1]-self.target.shape[1]/2)
-        #y_bottom = y_top + target.shape[1]
-        #x_left = min(0,center[0]-self.target.shape[0]/2)
-        #x_right = x_left + target.shape[0]
-        #target = img[y_top:y_bottom,x_left,x_right]
-        #self.target = cv2.GaussianBlur(target,(15,15),7)
+        
+        # Update self.target using new center.
+        self.target = getImageSection(self, self.center, self.target.shape)
+
+        #TODO: Update self.target in a way that accounts for size changes
+        #      in the target. Should we blend the new self.target above with
+        #      the old one to do this? How would that degrade over time?
+        
         return self.center
     
     def weigh_particles(self):
@@ -157,24 +173,14 @@ class Tracker:
         """
         weights = []
         for i in range(len(self.particles)):
-            # Construct our particle frame
-            #print i
-            p_t = int(self.particles[i][0] - np.floor(self.target.shape[0]/2.))
-            p_b = int(self.particles[i][0] + np.ceil(self.target.shape[0]/2.))
-            p_l = int(self.particles[i][1] - np.floor(self.target.shape[1]/2.))
-            p_r = int(self.particles[i][1] + np.ceil(self.target.shape[1]/2.))
-            try:
-                p_frame = self.img[p_t:p_b,p_l:p_r]
-            except:
+            p_frame = self.getImageSection(self, self.particles[i], 
+                                           self.target.shape)
+            if p_frame == False:
                 weights.append(0.)
                 continue
             
-            if p_frame.shape <> self.target.shape:    # Frame is off the image
-                weights.append(0.)
-            else: 
-                similarity = self.compareMSE(p_frame, self.target)
-                weights.append(similarity)
+            similarity = self.compareMSE(p_frame, self.target)
+            weights.append(similarity)
         
         self.maxrw = np.max(weights)
         return self.normWeights(weights)
-
